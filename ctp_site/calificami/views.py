ec428d902539c31a8b5741a0b808ec50b8cb62ba
Federico Olivares <119693761+FedeOlivares@users.noreply.github.com>
2023-08-01 20:34:07 +0000

    Author:    Federico Olivares <119693761+FedeOlivares@users.noreply.github.com>  On branch experimental-Django  Your branch is up to date with 'origin/experimental-Django'. 	modified:   ctp_site/calificami/__pycache__/views.cpython-310.pyc 	new file:   ctp_site/calificami/static/calificami/css/styles.css 	new file:   ctp_site/calificami/templates/calificami/about.html 	new file:   ctp_site/calificami/templates/calificami/index.html 	new file:   ctp_site/calificami/templates/layout.html 	modified:   ctp_site/calificami/views.py 	modified:   ctp_site/ctp_site/__pycache__/urls.cpython-310.pyc 	modified:   ctp_site/ctp_site/urls.py

--- ctp_site/calificami/views.py
+++ ctp_site/calificami/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request): 
-    return HttpResponse('Welcome! to CTD')
+    context = { 'title' : 'Index',
+    }
+    return render(request, 'calificami/index.html', context)
 
 def aboutus(request): 
     return HttpResponse('Welcome! to CTDs about us!')
