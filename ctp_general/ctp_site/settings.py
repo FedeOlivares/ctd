961d7716a0412a6efe3511fd2e7e1fd533af484a
Federico Olivares <119693761+FedeOlivares@users.noreply.github.com>
2023-11-02 21:23:47 +0000

    Changes to be committed: 	renamed:    ctp_general/calificami/templates/calificami/login.html -> ctp_general/calificami/templates/calificami/DEPRlogin.html 	renamed:    ctp_general/calificami/templates/calificami/DEPRECIATEDregister.html -> ctp_general/calificami/templates/calificami/DEPRregister.html 	modified:   ctp_general/calificami/templates/layout.html 	modified:   ctp_general/calificami/urls.py 	modified:   ctp_general/calificami/views.py 	modified:   ctp_general/ctp_site/settings.py 	modified:   ctp_general/ctp_site/urls.py 	modified:   ctp_general/db.sqlite3 	new file:   ctp_general/users/templates/users/login.html 	modified:   ctp_general/users/templates/users/register.html
        Deprecated old Log in and Register page. Added new Log in Page which
        logs in to home.

--- ctp_general/ctp_site/settings.py
+++ ctp_general/ctp_site/settings.py
@@ -131,3 +131,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = 'ctd-home'
--- ctp_general/ctp_site/urls.py
+++ ctp_general/ctp_site/urls.py
@@ -22,6 +22,8 @@ from users import views as user_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('calificami.urls')),
-    path("register/", user_views.register, name="register")
+    path("register/", user_views.register, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name = 'users/login.html'), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name="logout"),
 
 ]
