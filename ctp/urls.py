5f6cbb12e79de3a9564ce0347e0a3545669b878f
Federico Olivares <119693761+FedeOlivares@users.noreply.github.com>
2024-09-21 11:56:02 +0000

    Simplify naming conventions

--- ctp/urls.py
+++ ctp/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ctp_site project.
+URL configuration for ctp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- ctp/wsgi.py
+++ ctp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ctp_site project.
+WSGI config for ctp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ctp_site.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ctp.settings")
 
 application = get_wsgi_application()
